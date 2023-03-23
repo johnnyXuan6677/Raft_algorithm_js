fa870fab8428cad38d6aee6cd2c42e5b6e7653ae
johnnyXuan6677 <johnny246810@gmail.com>
2023-03-23 21:40:10 +0800

    5 node

---
+++ node test.js
@@ -0,0 +1,53 @@
+
+var skiff = require('./skiff.js');
+var argv = require('argh').argv;
+var peerarray = [];
+var portnum=argv.port;
+//port setup    node example --port 1000 -->for 1000 port to setup
+function peeradd(portnum){
+  for(let i = 0; i < portnum;i++){
+      peerarray.push('/ip4/127.0.0.1/tcp/'+(1000+i))
+  }}
+peeradd(portnum);
+
+var options = {
+  db: require('memdown'), // in memory database
+  peers:peerarray
+  // network:skiff.createNetwork(5000)
+}
+//port unuse detector 
+var nodeaddress
+
+
+
+// Skiff library for raft algorithm setup
+var Skiff = skiff('/ip4/127.0.0.1/tcp/9490',options)
+const db = Skiff.levelup()
+Skiff.start(err => {
+  if (err) {
+    console.error('Error starting skiff node: ', err.message)
+  } else {
+    console.log('Skiff node started')
+
+    db.put('key', 'value', (err) => {
+      // ...
+    })
+  }
+})
+
+Skiff.term((data)=> console.log(data))
+Skiff.stats((err)=> console.log(err))
+Skiff.join('/ip4/127.0.0.1/tcp/9490',(callback) => {console.log("join started   "+callback)})
+
+Skiff.on('started', (err) => {
+  if (err){
+    console.log(' some thing wrong'+err.message)
+}else{
+  console.log("the node is started !")
+  console.log("====================================")
+}})
+Skiff.on('new state',(state) => console.log("I am "+state+' now'))
+Skiff.on('joined',(add)=> console.log("new node join !!       "+add))
+Skiff.on('connect',peer =>console.log("connect event:   "+peer))
+// Skiff.on('rpc latency',(rpc)=> console.log("RPC latency   "+rpc))
+
---
+++ node2.js
@@ -0,0 +1,52 @@
+var skiff = require('./skiff.js');
+var options = {
+  db: require('memdown'), // in memory database
+  peers: [ // peer addresses
+    '/ip4/127.0.0.1/tcp/9490',
+    '/ip4/127.0.0.1/tcp/9491',
+    '/ip4/127.0.0.1/tcp/9492',
+    '/ip4/127.0.0.1/tcp/9493',
+    '/ip4/127.0.0.1/tcp/9494'
+
+  ],
+  
+}
+
+
+var Skiff = skiff('/ip4/127.0.0.1/tcp/9491',options
+);
+
+
+const db = Skiff.levelup()
+Skiff.start(err => {
+  if (err) {
+    console.error('Error starting skiff node: ', err.message)
+  } else {
+    console.log('Skiff node started')
+
+    db.put('key', 'value', (err) => {
+      // ...
+    })
+  }
+})
+
+// '/ip4/0.0.0.0/tcp/9163'
+Skiff.join('/ip4/127.0.0.1/tcp/9491',(callback) => {console.log("join started   "+callback)})
+Skiff.stats((callback) => {console.log("status!!:   "+callback)})
+
+// Skiff.leveldown((err)=> console.log("hello!!  "+ err))
+Skiff.readConsensus((err)=> console.log(err))
+Skiff.stats((err)=> console.log(err))
+
+
+
+Skiff.on('started', (err) => {
+  if (err){
+    console.log(' some thing wrong'+err.message)
+}else{
+  console.log("the node is started ")
+  console.log("====================================")
+}})
+Skiff.on('new state',(state) => console.log("I am "+state+' now'))
+// Skiff.on('rpc latency',(rpc)=> console.log("RPC latency   "+rpc))
+Skiff.on('warning',(err)=> console.log("non fatal error:  "+ err))
